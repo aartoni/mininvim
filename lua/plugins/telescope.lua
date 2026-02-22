4a539084ce42f1079e9ca09c97c9b72562d9dd52
aartoni <alessio@artoni.org>
2026-02-22 15:23:26 +0100

    feat: update telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
+    tag = "v0.2.1",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         require("telescope").setup({})
 
