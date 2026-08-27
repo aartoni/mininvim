297a2311e80390d7e5356827192b00b99b80e506
aartoni <alessio@artoni.org>
2026-08-27 16:06:47 +0200

    feat: simplify fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,33 +1,25 @@
+local buf_maps = {
+    { "<leader>p", function() vim.cmd.Git("push") end },
+    { "<leader>P", function() vim.cmd.Git({ "pull", "--rebase" }) end },
+}
+
 return {
     "tpope/vim-fugitive",
+    cmd = { "Git", "G" },
+    keys = {
+        { "<leader>gs", vim.cmd.Git, desc = "Git status" },
+        { "gu", "<cmd>diffget //2<CR>", desc = "Git diffget target" },
+        { "gh", "<cmd>diffget //3<CR>", desc = "Git diffget merge" },
+    },
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
-        local FugitiveGroup = vim.api.nvim_create_augroup("Fugitive", {})
-
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("FileType", {
-            group = FugitiveGroup,
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("Fugitive", {}),
             pattern = "fugitive",
             callback = function(args)
-                local bufnr = args.buf
-                local opts = { buffer = bufnr, remap = false }
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git("push")
-                end, opts)
-
-                -- rebase always
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({ "pull", "--rebase" })
-                end, opts)
-
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+                for _, map in ipairs(buf_maps) do
+                    vim.keymap.set("n", map[1], map[2], { buffer = args.buf })
+                end
             end,
         })
-
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
     end,
 }
