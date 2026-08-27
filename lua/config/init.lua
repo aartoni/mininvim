65ceef629d22de38ceaefecc1c910907587aa4ec
aartoni <alessio@artoni.org>
2026-08-27 14:46:34 +0200

    feat: simplify keymaps

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -24,40 +24,25 @@ autocmd({ "BufWritePre" }, {
     command = [[%s/\s\+$//e]],
 })
 
+local lsp_maps = {
+    { "gd", vim.lsp.buf.definition },
+    { "K", vim.lsp.buf.hover },
+    { "<leader>vws", vim.lsp.buf.workspace_symbol },
+    { "<leader>vd", vim.diagnostic.open_float },
+    { "<leader>vca", vim.lsp.buf.code_action },
+    { "<leader>vrr", vim.lsp.buf.references },
+    { "<leader>vrn", vim.lsp.buf.rename },
+    { "<C-h>", vim.lsp.buf.signature_help, mode = "i" },
+    { "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end },
+    { "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end },
+}
+
 autocmd("LspAttach", {
     group = mininvim_group,
     callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function()
-            vim.lsp.buf.definition()
-        end, opts)
-        vim.keymap.set("n", "K", function()
-            vim.lsp.buf.hover()
-        end, opts)
-        vim.keymap.set("n", "<leader>vws", function()
-            vim.lsp.buf.workspace_symbol()
-        end, opts)
-        vim.keymap.set("n", "<leader>vd", function()
-            vim.diagnostic.open_float()
-        end, opts)
-        vim.keymap.set("n", "<leader>vca", function()
-            vim.lsp.buf.code_action()
-        end, opts)
-        vim.keymap.set("n", "<leader>vrr", function()
-            vim.lsp.buf.references()
-        end, opts)
-        vim.keymap.set("n", "<leader>vrn", function()
-            vim.lsp.buf.rename()
-        end, opts)
-        vim.keymap.set("i", "<C-h>", function()
-            vim.lsp.buf.signature_help()
-        end, opts)
-        vim.keymap.set("n", "]d", function()
-            vim.diagnostic.jump({ count = 1, float = true })
-        end, opts)
-        vim.keymap.set("n", "[d", function()
-            vim.diagnostic.jump({ count = -1, float = true })
-        end, opts)
+        for _, map in ipairs(lsp_maps) do
+            vim.keymap.set(map.mode or "n", map[1], map[2], { buffer = e.buf })
+        end
     end,
 })
 
