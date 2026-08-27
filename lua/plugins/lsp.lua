00bd9d5838b6d3c122f138fb23a2503c1d4ab6db
aartoni <alessio@artoni.org>
2026-08-27 17:00:52 +0200

    feat: decouple LSP and completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,16 +5,10 @@ return {
         "mason-org/mason.nvim",
         "mason-org/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
-
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-        local cmp = require("cmp")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         vim.lsp.config("*", { capabilities = capabilities })
 
@@ -24,33 +18,7 @@ return {
 
         require("fidget").setup({})
         require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-            },
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-            }, {
-                { name = "buffer" },
-            }),
-        })
+        require("mason-lspconfig").setup({ ensure_installed = { "lua_ls" } })
 
         vim.diagnostic.config({
             float = {
