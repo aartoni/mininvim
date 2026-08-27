4404873238f24ef4890a53ebc338f8caccf79769
aartoni <alessio@artoni.org>
2026-08-27 15:45:09 +0200

    ref: simplify formatting config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,30 +1,26 @@
 return {
     "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {},
-    config = function()
-        require("conform").setup({
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
-            formatters_by_ft = {
-                c = { "clang-format" },
-                cpp = { "clang-format" },
-                elixir = { "mix" },
-                go = { "gofmt" },
-                javascript = { "prettier" },
-                lua = { "stylua" },
-                rust = { "rustfmt" },
-                typescript = { "prettier" },
-            },
-            formatters = {
-                ["clang-format"] = { prepend_args = { "-style=file" } },
-            },
-        })
-
-        vim.keymap.set("n", "<leader>f", function()
-            require("conform").format()
-        end)
-    end,
+    event = "BufWritePre",
+    keys = {
+        { "<leader>f", function() require("conform").format() end },
+    },
+    opts = {
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+        formatters_by_ft = {
+            c = { "clang-format" },
+            cpp = { "clang-format" },
+            elixir = { "mix" },
+            go = { "gofmt" },
+            javascript = { "prettier" },
+            lua = { "stylua" },
+            rust = { "rustfmt" },
+            typescript = { "prettier" },
+        },
+        formatters = {
+            ["clang-format"] = { prepend_args = { "-style=file" } },
+        },
+    },
 }
